47756873b943affa45de1e4688d82ec45545396d
Jingyu <381258337@qq.com>
2022-09-15 16:27:18 +0800

    update chessboard

--- matrices/grid3dchessboard/Grid3dchessboardMat.m
+++ matrices/grid3dchessboard/Grid3dchessboardMat.m
@@ -18,9 +18,9 @@ II = speye(n^2,n^2);
 Apattern = blockdiags ([-II aII -II],-1:1,n,n);
 [row,col] = find(Apattern);
 
-% Disturbance
+% Chessboard.
 N = n^3;
-A = sparse(N,N);
+offset = n / 8;
 B = 0.1 * speye(N);
 
 ivec = zeros(7*N,1);
@@ -33,13 +33,19 @@ for it =1:length(row)
     coli = col(it);
     if rowi ~= coli
         [i1,j1,k1] = Idx1d(rowi,n,n,n);
+        i1 = floor((i1-1)/offset);
+        j1 = floor((j1-1)/offset);
+        k1 = floor((k1-1)/offset);
         [i2,j2,k2] = Idx1d(coli,n,n,n);
-        if mod(i1 + j1 + k1, 14) < 7
+        i2 = floor((i2-1)/offset);
+        j2 = floor((j2-1)/offset);
+        k2 = floor((k2-1)/offset);
+        if mod(i1 + j1 + k1, 2) == 0
             a1 = 1000;
         else
             a1 = 0.1;
         end
-        if mod(i2 + j2 + k2, 14) < 7
+        if mod(i2 + j2 + k2, 2) == 0
             a2 = 1000;
         else
             a2 = 0.1;
@@ -53,7 +59,7 @@ for it =1:length(row)
 end
 A = sparse(ivec(1:itt),jvec(1:itt),vvec(1:itt));
 dA = sparse(1:N,1:N,sum(A,2));
-A = A-dA;
+A = A - dA;
 
 A = A + B;
 
